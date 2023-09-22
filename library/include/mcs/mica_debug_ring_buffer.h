e556f7879c415149e09401a6424430e10c7fab57
Yongmao Luo <luoyongmao@huawei.com>
2023-09-22 17:31:27 +0800

    feat: debug RTOS based on MICA (ring buffer)
    - the ring_buffer.h file contains the definitions and operations
    of the ring buffer.
    
    - the mica_debug_ring_buffer part of data contains the data structure
    and interfaces for sending and receiving messages to and from the proxy
    server module. This part of code calls the functions in ring_buffer.h
    to directly manipulate the shared memory.
    
    The reason why we have independent files for ring buffer
    operations is because it is easy for us to extend the software
    architecture to support other communication mechanisms based on
    shared memory.
    
    Signed-off-by: Yongmao Luo <luoyongmao@huawei.com>

---
+++ library/include/mcs/mica_debug_ring_buffer.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#ifndef MICA_DEBUG_RING_BUFFER_H
+#define MICA_DEBUG_RING_BUFFER_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <mqueue.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "mcs_common.h"
+#include "mica_debug_common.h"
+#include "openamp_module.h"
+#include "ring_buffer.h"
+
+/* the shared memory space used for communication */
+#define RING_BUFFER_PA 0x400000000 - 0x4000
+#define RING_BUFFER_LEN 0x1000
+
+struct debug_ring_buffer_module_data{
+    void *rx_buffer;
+    void *tx_buffer;
+    int len;
+    mqd_t to_server;
+    mqd_t from_server;
+    pthread_t data_to_rtos_thread;
+    pthread_t data_to_server_thread;
+};
+
+/* Deliver packets from server to RTOS */
+int transfer_data_to_rtos(struct debug_ring_buffer_module_data *ring_buffer_module_data);
+static void *data_to_rtos_thread(void *args);
+/* Deliver packets from RTOS to server */
+int transfer_data_to_server(struct debug_ring_buffer_module_data *ring_buffer_module_data);
+static void *data_to_server_thread(void *args);
+
+int start_ring_buffer_module(struct client_os_inst *client, mqd_t from_server, mqd_t to_server, struct debug_ring_buffer_module_data **ring_buffer_module_data_out);
+void free_resources_for_ring_buffer_module(struct debug_ring_buffer_module_data *ring_buffer_module_data);
+
+#endif
---
+++ library/include/mcs/ring_buffer.h
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+typedef struct ring_buffer {
+    int len;
+    volatile int busy; // lock
+    volatile int tail; // writer pos
+    volatile int head; // reader pos
+    char redzone[8];
+} ring_buffer_t;
+
+static void ring_buffer_lock(ring_buffer_t *ring_buffer)
+{
+    while (ring_buffer->busy) {
+        ;
+    }
+    ring_buffer->busy = 1;
+}
+
+static void ring_buffer_unlock(ring_buffer_t *ring_buffer)
+{
+    ring_buffer->busy = 0;
+}
+
+static ring_buffer_t * ring_buffer_init(void *addr, int len)
+{
+    ring_buffer_t *ring_buffer = (ring_buffer_t *)addr;
+    ring_buffer->busy = 0;
+    ring_buffer->len = len;
+    ring_buffer->tail = ring_buffer->head = 0;
+    for (int i = 0; i < sizeof(ring_buffer->redzone); i++) {
+        ring_buffer->redzone[i] = 0x7a;
+    }
+    return ring_buffer;
+}
+
+static int ring_buffer_pair_init(void *rxaddr, void *txaddr, int len)
+{
+    if (!rxaddr || !txaddr || len <= sizeof(ring_buffer_t)) {
+        return -1;
+    }
+    ring_buffer_init(rxaddr, len - sizeof(ring_buffer_t));
+    ring_buffer_init(txaddr, len - sizeof(ring_buffer_t));
+    return 0;
+}
+
+static inline int is_empty(ring_buffer_t *o)
+{
+    return o->head == o->tail;
+}
+
+static inline int is_full(ring_buffer_t *o)
+{
+    return (o->tail + 1) % o->len == o->head;
+}
+
+static int readable(ring_buffer_t *o)
+{
+    int ret;
+    ring_buffer_lock(o);
+    ret = !is_empty(o);
+    ring_buffer_unlock(o);
+    return ret;
+}
+
+static int writable(ring_buffer_t *o)
+{
+    int ret;
+    ring_buffer_lock(o);
+    ret = !is_full(o);
+    ring_buffer_unlock(o);
+    return ret;
+}
+
+static int ring_buffer_write(ring_buffer_t *ring_buffer, char *buf, int len)
+{
+    int olen = ring_buffer->len;
+    int cnt = 0;
+    char *obuf = (char *)ring_buffer + sizeof(ring_buffer_t);
+
+    ring_buffer_lock(ring_buffer);
+    while (cnt < len) {
+        if (is_full(ring_buffer)) {
+            break;
+        }
+        obuf[(ring_buffer->tail++) % olen] = buf[cnt++];
+    }
+    ring_buffer_unlock(ring_buffer);
+    
+    return cnt;
+}
+
+static int ring_buffer_read(ring_buffer_t *ring_buffer, char *buf, int len)
+{
+    int olen = ring_buffer->len;
+    char *obuf = (char *)ring_buffer + sizeof(ring_buffer_t);
+    int cnt = 0;
+    ring_buffer_lock(ring_buffer);
+    while (cnt < len) {
+        if (is_empty(ring_buffer)) {
+            break;
+        }
+        buf[cnt++] = obuf[(ring_buffer->head++) % olen];
+    }
+    ring_buffer_unlock(ring_buffer);
+    
+    return cnt;
+}
+
+#endif
