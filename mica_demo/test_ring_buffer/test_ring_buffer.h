ad18c6e2c8dd5413eadb11a56f9080142f0771df
Yongmao Luo <luoyongmao@huawei.com>
2023-09-22 17:36:42 +0800

    feat: debug RTOS based on MICA (testcase based on ring buffer)
    - the files contain the testcase which simulates the communication
      application on RTOS reading and writing data through the shared
      memory based on ring buffer mechanism.
    
    Since the purpose of testcases is to validate if the communication
    based on shared memory works properly, it is the same to have
    the testcase realized in either RTOS or the openEuler side, only if
    the testcase can act as a backend receiving and sending data.
    
    Signed-off-by: Yongmao Luo <luoyongmao@huawei.com>

---
+++ mica_demo/test_ring_buffer/test_ring_buffer.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#ifndef TEST_RING_BUFFER_H
+#define TEST_RING_BUFFER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <errno.h>
+
+#include "ring_buffer.h"
+#include "mica_debug_common.h"
+
+#define MCS_DEVICE_NAME    "/dev/mcs"
+
+#endif
