83eae9a675674831e10e887522d8552a2a70bdbf
Yongmao Luo <luoyongmao@huawei.com>
2023-09-25 17:55:54 +0800

    feat: debug RTOS based on MICA (mica_debug)
    - start a gdb proxy server module as the frontend facing the gdb client.
    
    - start a ring buffer transfer module as the backend interacting with
      remote RTOS through ring buffer on shared memory.
    
    - start a GDB subprocess to interactively debug the remote RTOS.
    
    The reason why we divide the transfer module into two parts is because
    it is easier for us to replace ring buffer with other shared memory communication
    mechanism under such software architecture.
    
    Signed-off-by: Yongmao Luo <luoyongmao@huawei.com>

---
+++ mica_demo/mica_debug.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved
+ *
+ * SPDX-License-Identifier: MulanPSL-2.0
+ */
+
+#ifndef MICA_DEBUG_H
+#define MICA_DEBUG_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <pthread.h>
+#include <mqueue.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#include "openamp_module.h"
+#include "mica_gdb_server.h"
+
+#ifdef CONFIG_RING_BUFFER
+#include "mica_debug_ring_buffer.h"
+#endif
+
+#define TO_SHARED_MEM_QUEUE_NAME "/to_shared_mem_queue"
+#define FROM_SHARED_MEM_QUEUE_NAME "/from_shared_mem_queue"
+#define MAX_QUEUE_SIZE 10
+#define MAX_PARAM_LENGTH 100
+
+/*
+    the message queues between proxy server and shared memory
+    transferred module.
+*/
+static int alloc_message_queue();
+static void free_message_queue();
+
+int debug_start(struct client_os_inst *client_os, char *elf_name);
+static void *server_loop_thread(void *arg);
+
+#endif
