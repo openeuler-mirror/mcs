5be881177c7e1c53cfc405e2cbc91d24fcacdb2d
Yongmao Luo <luoyongmao@huawei.com>
2023-09-25 17:29:35 +0800

    feat: debug RTOS based on MICA (rpmsg_pty)
    - fix bugs of not freeing resources if there has an error in the
      middle of functions. Now the rpmsg application can be started
      repeatedly rather than only once.
    
    - refactor the way of starting rpmsg pty.
    
    Now if the programmers want to start the pty, they can just call
    rpmsg_app_start.
    
    If they want to free resources, they should call rpmsg_app_stop.
    Do not forget to add while(1){} in the main thread since we start rpmsg
    pty applications in sub-threads.
    
    Signed-off-by: Yongmao Luo <luoyongmao@huawei.com>

--- mica_demo/rpmsg_pty.h
+++ mica_demo/rpmsg_pty.h
@@ -7,19 +7,41 @@
 #ifndef RPMSG_PTY_H
 #define RPMSG_PTY_H
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <pthread.h>
+
 #if defined __cplusplus
 extern "C" {
 #endif
 
 struct pty_ep_data {
 	unsigned int ep_id; /* endpoint id */
-
     int fd_master;  /* pty master fd */
-
     pthread_t pty_thread; /* thread id */
 };
 
-struct pty_ep_data * pty_service_create(const char* ep_name);
+struct rpmsg_app_resource {
+    struct pty_ep_data *pty_ep_uart;
+    struct pty_ep_data *pty_ep_console;
+    pthread_t rpmsg_loop_thread;
+};
+
+/* entrance for starting rpmsg applications */
+int rpmsg_app_start(struct client_os_inst *client);
+/* free resources for rpmsg apps */
+void rpmsg_app_stop(void);
+/* thread for polling the interrupt from RTOS side */
+static void *rpmsg_loop_thread(void *arg);
+/* create pty service based on openAMP */
+static struct pty_ep_data * pty_service_create(const char* ep_name);
+int open_pty(int *pfdm);
+static void *pty_thread(void *arg);
+/* The callback functions needed for RPMsg endpoint of pty service */
+static int pty_endpoint_cb(struct rpmsg_endpoint *ept, void *data,
+		size_t len, uint32_t src, void *priv);
+static void pty_endpoint_unbind_cb(struct rpmsg_endpoint *ept);
+static void pty_endpoint_exit(struct pty_ep_data *pty_ep);
 
 #if defined __cplusplus
 }
